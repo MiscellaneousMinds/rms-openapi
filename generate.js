f0db17167c495b3b87da1394c4884b80a1c31567
AdeThorMiwa <bendamyth@gmail.com>
2023-07-05 02:02:03 +0100

    force workflow trigger

--- generate.js
+++ generate.js
@@ -41,7 +41,7 @@ fi\n`;
       script += `echo "${rustRegistryName} = { index = \"${rustRegistryIndex}\" }" >> ./.cargo/config.toml\n`;
       script += `echo "[registries.${rustRegistryName}]" >> ./.cargo/credentials\n`;
       script += `echo "token = ${RUST_REGISTRY_API_KEY}" >> ./.cargo/credentials\n`;
-      script += `cargo publish --registry ${rustRegistryName}\n`;
+      script += `cargo publish --registry ${rustRegistryName}\n\n`;
       break;
     // eslint-disable-next-line no-fallthrough
     default:
